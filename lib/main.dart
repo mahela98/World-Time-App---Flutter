9b9a4552fea27808ca8396bd4844562f395bef12
mahela98 <chandimaofficial@gmail.com>
2021-08-18 08:46:55 +0530

    created routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:first_full_app/pages/choose_location.dart';
 import 'package:first_full_app/pages/home.dart';
+import 'package:first_full_app/pages/loading.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  initialRoute: '/home',
+  routes: {
+    '/': (context) =>Loading(),
+    '/home':(context)=> Home(),
+    '/location': (context)=> ChooseLocation(),
+
+  },
 ));
