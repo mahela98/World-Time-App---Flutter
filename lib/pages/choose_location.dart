c0bf9ac871f18a5a85fde7e33b9242292170df21
mahela98 <chandimaofficial@gmail.com>
2021-08-25 09:17:48 +0530

    youtube complete

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -13,6 +13,21 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(location: 'Sl', flag: 'London', url: 'Europe/Africa'),
   ];
 
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    print(instance.url);
+    await instance.getTime();
+    //navigate home screen
+    Navigator.pop(context,{
+      'location':instance.location,
+      'flag':instance.flag,
+      'time':instance.time,
+      'isDaytime':instance.isDaytime,
+    });
+    
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,23 +40,23 @@ class _ChooseLocationState extends State<ChooseLocation> {
       ),
       body: ListView.builder(
         itemCount: locations.length,
-        itemBuilder: (context,index){
+        itemBuilder: (context, index) {
           return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 1,horizontal: 4),
+            padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
             child: Card(
               child: ListTile(
-                onTap: (){
-                  print(locations[index]);
+                onTap: () {
+                  updateTime(index);
                 },
                 title: Text(locations[index].location),
                 leading: CircleAvatar(
-                  backgroundImage: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Flag_of_Sri_Lanka.svg/800px-Flag_of_Sri_Lanka.svg.png'),
+                  backgroundImage: NetworkImage(
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Flag_of_Sri_Lanka.svg/800px-Flag_of_Sri_Lanka.svg.png'),
                 ),
               ),
             ),
           );
         },
-
       ),
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,7 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context)!.settings.arguments as Map<dynamic, dynamic>;
+    data = data.isNotEmpty? data: ModalRoute.of(context)!.settings.arguments as Map<dynamic, dynamic>;
     print(data);
 
     //set background
@@ -34,8 +34,17 @@ class _HomeState extends State<Home> {
             child: Column(
               children: [
                 TextButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/location');
+                  onPressed: () async {
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
+                    setState(() {
+                      data = {
+                        'time': result['time'],
+                        'location': result['location'],
+                        'isDaytime': result['isDaytime'],
+                        'flag': result['flag']
+                      };
+                    });
                   },
                   icon: Icon(
                     Icons.edit_location,
@@ -73,8 +82,18 @@ class _HomeState extends State<Home> {
                     color: textColor,
                   ),
                 ),
+                SizedBox(height: 70,),
+                // ElevatedButton.icon(
+                //     onPressed: (){},
+                //     icon: Icon(
+                //       Icons.edit_location,
+                //       color: Colors.blue[100],
+                //     ),
+                //   label: Text('Choose Location'),
+                // ),
               ],
             ),
+
           ),
         ),
       ),
